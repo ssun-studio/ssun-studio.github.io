d9a80c07981718ce15dfba6a6c8f0003933a97f9
Seavmey Sun <79233177+ssun-studio@users.noreply.github.com>
2023-12-04 03:29:07 -0500

    Add files via upload

---
+++ assets/js/api.js
@@ -0,0 +1,34 @@
+/**
+ * @license MIT
+ * @copyright 2023 codewithsadee
+ * @author codewithsadee <mohammadsadee24@gmail.com>
+ */
+
+"use strict";
+
+
+window.ACCESS_POINT = "https://api.edamam.com/api/recipes/v2";
+const /** {String} */ APP_ID = "141dd413";
+const /** {String} */ API_KEY = "74592accdf45138e5fc868613795cae0";
+const /** {String} */ TYPE = "public";
+
+/**
+ * @param {Array} queries Query array
+ * @param {Function} successCallback Success callback function
+ */
+
+export const fetchData = async function (queries, successCallback) {
+  const /** {String} */ query = queries?.join("&")
+    .replace(/,/g, "=")
+    .replace(/ /g, "%20")
+    .replace(/\+/g, "%2B");
+
+  const /** {String} */ url = `${ACCESS_POINT}?app_id=${APP_ID}&app_key=${API_KEY}&type=${TYPE}${query ? `&${query}` : ""}`;
+
+  const /** {Object} */ response = await fetch(url);
+
+  if (response.ok) {
+    const data = await response.json();
+    successCallback(data);
+  }
+}
